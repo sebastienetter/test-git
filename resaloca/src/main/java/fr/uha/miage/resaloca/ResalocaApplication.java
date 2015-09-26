ae241d84d5b7c5662a7e652c246a35fb74d06b4b
setter <sebastien.etter@stemys.ch>
2015-09-26 09:54:15 +0200

    Ajout d'un commentaire

--- resaloca/src/main/java/fr/uha/miage/resaloca/ResalocaApplication.java
+++ resaloca/src/main/java/fr/uha/miage/resaloca/ResalocaApplication.java
@@ -3,6 +3,8 @@ package fr.uha.miage.resaloca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Ceci est un commentaire 
+
 @SpringBootApplication
 public class ResalocaApplication {
 
