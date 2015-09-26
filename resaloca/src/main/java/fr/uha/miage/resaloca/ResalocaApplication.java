3d9e6abf10c0cf735ec05e8c0665307919f22c3f
setter <sebastien.etter@stemys.ch>
2015-09-26 09:27:30 +0200

    First commit

---
+++ resaloca/src/main/java/fr/uha/miage/resaloca/ResalocaApplication.java
@@ -0,0 +1,12 @@
+package fr.uha.miage.resaloca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResalocaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResalocaApplication.class, args);
+    }
+}
